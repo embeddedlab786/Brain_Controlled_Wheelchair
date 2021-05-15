b0cb3142d2e9745f3a4811659d4621b5a3aba180
Muhammad Ansar <57707946+embeddedlab786@users.noreply.github.com>
2021-05-14 18:39:30 -0700

    Add files via upload

---
+++ Brain_Controlled_Wheelchair.ino
@@ -0,0 +1,206 @@
+#define LED 13
+#define BAUDRATE 57600
+#define DEBUGOUTPUT 0
+
+#define enA 5//Enable1 L298 Pin enA 
+#define in1 6 //Motor1  L298 Pin in1 
+#define in2 7 //Motor1  L298 Pin in1 
+#define in3 8 //Motor2  L298 Pin in1 
+#define in4 9 //Motor2  L298 Pin in1 
+#define enB 10 //Enable2 L298 Pin enB 
+
+#define powercontrol 10
+
+// checksum variables
+byte generatedChecksum = 0;
+byte checksum = 0; 
+int payloadLength = 0;
+byte payloadData[64] = {0};
+byte poorQuality = 0;
+byte attention = 0;
+byte meditation = 0;
+
+// system variables
+long lastReceivedPacket = 0;
+boolean bigPacket = false;
+
+int Speed = 130;  
+
+//////////////////////////
+// Microprocessor Setup //
+//////////////////////////
+void setup() {// put your setup code here, to run once
+pinMode(enA, OUTPUT); // declare as output for L298 Pin enA 
+pinMode(in1, OUTPUT); // declare as output for L298 Pin in1 
+pinMode(in2, OUTPUT); // declare as output for L298 Pin in2 
+pinMode(in3, OUTPUT); // declare as output for L298 Pin in3   
+pinMode(in4, OUTPUT); // declare as output for L298 Pin in4 
+pinMode(enB, OUTPUT); // declare as output for L298 Pin enB 
+
+analogWrite(enA, 130); // Write The Duty Cycle 0 to 255 Enable Pin A for Motor1 Speed 
+analogWrite(enB, 130); // Write The Duty Cycle 0 to 255 Enable Pin B for Motor2 Speed
+
+  pinMode(LED, OUTPUT); 
+ 
+  Serial.begin(BAUDRATE);           // USB
+}
+
+////////////////////////////////
+// Read data from Serial UART //
+////////////////////////////////
+byte ReadOneByte() 
+
+{
+  int ByteRead;
+  while(!Serial.available());
+  ByteRead = Serial.read();
+
+#if DEBUGOUTPUT  
+  Serial.print((char)ByteRead);   // echo the same byte out the USB serial (for debug purposes)
+#endif
+
+  return ByteRead;
+}
+
+/////////////
+//MAIN LOOP//
+/////////////
+void loop() {
+  // Look for sync bytes
+  if(ReadOneByte() == 170) 
+  {
+    if(ReadOneByte() == 170) 
+    {
+        payloadLength = ReadOneByte();
+      
+        if(payloadLength > 169)                      //Payload length can not be greater than 169
+        return;
+        generatedChecksum = 0;        
+        for(int i = 0; i < payloadLength; i++) 
+        {  
+        payloadData[i] = ReadOneByte();            //Read payload into memory
+        generatedChecksum += payloadData[i];
+        }   
+
+        checksum = ReadOneByte();                      //Read checksum byte from stream      
+        generatedChecksum = 255 - generatedChecksum;   //Take one's compliment of generated checksum
+
+        if(checksum == generatedChecksum) 
+        {    
+          poorQuality = 200;
+          attention = 0;
+          meditation = 0;
+
+          for(int i = 0; i < payloadLength; i++) 
+          {                                          // Parse the payload
+          switch (payloadData[i]) 
+          {
+          case 2:
+            i++;            
+            poorQuality = payloadData[i];
+            bigPacket = true;            
+            break;
+          case 4:
+            i++;
+            attention = payloadData[i];                        
+            break;
+          case 5:
+            i++;
+            meditation = payloadData[i];
+            break;
+          case 0x80:
+            i = i + 3;
+            break;
+          case 0x83:
+            i = i + 25;      
+            break;
+          default:
+            break;
+          } // switch
+        } // for loop
+
+#if !DEBUGOUTPUT
+
+        // *** Add your code here ***
+
+        if(bigPacket) 
+        {
+          if(poorQuality == 0)
+             digitalWrite(LED, HIGH);
+          else
+            digitalWrite(LED, LOW);
+          
+          Serial.print("PoorQuality: ");
+          Serial.print(poorQuality, DEC);
+          Serial.print(" Attention: ");
+          Serial.print(attention, DEC);
+          Serial.print(" Time since last packet: ");
+          Serial.print(millis() - lastReceivedPacket, DEC);
+          lastReceivedPacket = millis();
+          Serial.print("\n");
+if(attention>40 && attention<60){
+forword();         
+}
+
+if(attention>60 && attention<70){
+backword();
+}
+
+if(attention>30 && attention<40){
+turnLeft();
+}
+
+if(attention>20 && attention<30){            
+turnRight();           
+}
+
+if(attention<20 || attention>70){
+Stop();            
+}              
+        }
+#endif        
+        bigPacket = false;        
+      }
+      else {
+        // Checksum Error
+      }  // end if else for checksum
+    } // end if read 0xAA byte
+  } // end if read 0xAA byte
+}
+
+
+
+void forword(){  //forword
+digitalWrite(in1, HIGH); //Right Motor forword Pin 
+digitalWrite(in2, LOW);  //Right Motor backword Pin 
+digitalWrite(in3, LOW);  //Left Motor backword Pin 
+digitalWrite(in4, HIGH); //Left Motor forword Pin 
+}
+
+void backword(){ //backword
+digitalWrite(in1, LOW);  //Right Motor forword Pin 
+digitalWrite(in2, HIGH); //Right Motor backword Pin 
+digitalWrite(in3, HIGH); //Left Motor backword Pin 
+digitalWrite(in4, LOW);  //Left Motor forword Pin 
+}
+
+void turnRight(){ //turnRight
+digitalWrite(in1, LOW);  //Right Motor forword Pin 
+digitalWrite(in2, HIGH); //Right Motor backword Pin  
+digitalWrite(in3, LOW);  //Left Motor backword Pin 
+digitalWrite(in4, HIGH); //Left Motor forword Pin 
+}
+
+void turnLeft(){ //turnLeft
+digitalWrite(in1, HIGH); //Right Motor forword Pin 
+digitalWrite(in2, LOW);  //Right Motor backword Pin 
+digitalWrite(in3, HIGH); //Left Motor backword Pin 
+digitalWrite(in4, LOW);  //Left Motor forword Pin 
+}
+
+void Stop(){ //stop
+digitalWrite(in1, LOW); //Right Motor forword Pin 
+digitalWrite(in2, LOW); //Right Motor backword Pin 
+digitalWrite(in3, LOW); //Left Motor backword Pin 
+digitalWrite(in4, LOW); //Left Motor forword Pin 
+}
